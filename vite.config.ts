1e4a5ed8dd5bada4e56e73d4fd310ffc056f3299
WithoutAName25 <withoutaname@withoutaname.eu>
2022-03-25 14:39:49 +0100

    Fix configuration of VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -44,29 +44,9 @@ export default defineConfig({
 
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "safari-pinned-tab.svg"],
       manifest: {
-        name: "Vitesse",
-        short_name: "Vitesse",
+        name: "Mineplay",
         theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
       },
     }),
   ],
